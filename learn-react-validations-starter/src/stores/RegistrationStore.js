18e76cfd9d03a2b0a9302cc0c660a60d7e8e69d6
EricGelencserDev <33365941+EricGelencserDev@users.noreply.github.com>
2018-01-09 16:27:37 -0800

    Update RegistrationStore.js
    updated the registration code

--- learn-react-validations-starter/src/stores/RegistrationStore.js
+++ learn-react-validations-starter/src/stores/RegistrationStore.js
@@ -35,9 +35,36 @@ class RegistrationStore{
   validatePresence(fieldName){
     if(this.fields[fieldName] === ''){
       this.addError(fieldName, 'is Required')
+    }
+  }
+
+  validateLength(fieldName){
+  if(this.fields[fieldName].length <= 8 ){
+    this.addError(fieldName, 'Password needs to be more than 8 characters long!')
+  }
+}
+
+  validateAlphaNum(fieldName){
+      const filter = / d{1} /
+    if(!filter.test(this.fields[fieldName])){
+        this.addError(fieldName, 'Password needs to contain both letters and numbers.')
+  }
+}
+
+  validateEmail(fieldName){
+    const filter = /^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/
+    if(!filter.test(this.fields[fieldName])){
+      this.addError(fieldName, 'is not an email')
+    }
+  }
+
+  addError(fieldName, message){
+    this.errors[fieldName] = [
         message
     ]
   }
 }
+
+
 const store = new RegistrationStore()
 export default store
