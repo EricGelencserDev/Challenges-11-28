d0daf4c683dd6699f491caedb4234f5d21c494d1
Eric Gelencser <Ericgelencsermusic@hotmail.com>
2018-01-22 20:30:05 -0800

    onClick function, ship placement

--- ReactShip/battleship/src/components/board.js
+++ ReactShip/battleship/src/components/board.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-const ship = 1
+const shipSize = [5,4,3,3,2]; // store ship sizes in an array
+
 class Board extends Component {
     constructor(props){
         super(props)
-        this.state ={
+        this.state = {
             board: [],
         }
         this.setupBoard()
@@ -17,28 +18,38 @@ class Board extends Component {
         }
     }
 
-    placeShip(){
-        var col = Math.floor(Math.random()* 10)
-        var row = Math.floor(Math.random()* 10)
-        this.state.board[col][row] = ship
-    }
+    placeShip(ship){ // TODO needs to add vertical ship placement
+        var maxCol = 10 - shipSize[ship];
+        var col = Math.floor(Math.random()*maxCol);
+        var row = ship;
+        for (var shipSquare = 0;
+          shipSquare < shipSize[ship]; shipSquare++) { this.state.board[col+shipSquare][row] = 1; }
+        }
+
 
     fiveShips(){
         for(var i=0; i < 5; i++){
-            this.placeShip()
+            this.placeShip(i)
         }
     }
 
     clickHandler(col, row){
-        
+      if(this.state.board[col][row] === 1){
+        this.state.board[col][row] = 0
+          alert('Hit!')
+      }
+      else {
+        this.state.board[col][row]= 'x'
+      }
+      this.setState(this.state)
     }
 
 
     renderRow(rowNumber){
         var row = []
         for(var i=0; i < 10; i++){
-            row.push(<td id={`${i}_${rowNumber}`} onClick={this.clickHandler(i, rowNumber)}
->{this.state.board[i][rowNumber]}</td>)
+            row.push(<td id={`${i}_${rowNumber}`} onClick={this.clickHandler.bind(this, i, rowNumber)}>
+            {this.state.board[i][rowNumber]}</td>)  // forgot to add .bind the on click was being exicuted everytime the row was created
 
         }
         return (<tr>{row}</tr>)
